3518f2215ad67586eb7c26f3bbaeb4beb6fb992e
Stefano Garzarella <sgarzare@redhat.com>
2020-01-16 18:24:27 +0100

    vsock/virtio_transport_common: handle netns of received packets
    This patch allows transports that use virtio_transport_common
    to specify the network namespace where a received packet is to
    be delivered.
    
    virtio_transport and vhost_transport, for now, use the default
    network namespace.
    
    vsock_loopback uses the same network namespace of the transmitter.
    
    Signed-off-by: Stefano Garzarella <sgarzare@redhat.com>

--- drivers/vhost/vsock.c
+++ drivers/vhost/vsock.c
@@ -525,6 +525,7 @@ static void vhost_vsock_handle_tx_kick(struct vhost_work *work)
 			continue;
 		}
 
+		VIRTIO_VSOCK_SKB_CB(skb)->net = vsock_default_net();
 		total_len += sizeof(*hdr) + skb->len;
 
 		/* Deliver to monitoring devices all received packets */
